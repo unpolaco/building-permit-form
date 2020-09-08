21fabefecf37290e1507984644ec8245cb035aca
Michal Borowski <unpolacoo@gmail.com>
2020-09-08 11:32:31 +0200

    Create fields array with data for render components

---
+++ src/assets/FieldsData.js
@@ -0,0 +1,16 @@
+ const fields = [
+  {name: 'name', labelText: 'imię i nazwisko lub nazwa inwestora', classInput: 'long'},
+  {name: 'country', labelText: 'kraj'},
+  {name: 'voivodeship', labelText: 'województwo'},
+  {name: 'county', labelText: 'powiat'},
+  {name: 'community', labelText: 'gmina'},
+  {name: 'city', labelText: 'miejscowość'},
+  {name: 'street', labelText: 'ulica'},
+  {name: 'buildingNumber', labelText: 'nr budynku', classInput: 'short'},
+  {name: 'local number', labelText: 'nr lokalu', classInput: 'short'},
+  {name: 'postalCode', labelText: 'kod pocztowy', classInput: 'short'},
+  {name: 'phone', labelText: 'telefon'},
+  {name: 'email', labelText: 'e-mail'},
+  ];
+
+  export default fields;
