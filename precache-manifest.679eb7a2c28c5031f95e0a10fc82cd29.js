4391b0b0cd074368b7f784751ee5bf7b56784f8a
Michal Borowski <unpolacoo@gmail.com>
2020-09-09 10:35:04 +0200

    Updates

---
+++ precache-manifest.679eb7a2c28c5031f95e0a10fc82cd29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8371e166ed5a030beac12b91dbc02be",
+    "url": "/building-permit-form/index.html"
+  },
+  {
+    "revision": "cc6656a056e0b7a0bcf3",
+    "url": "/building-permit-form/static/css/main.2a3c9e29.chunk.css"
+  },
+  {
+    "revision": "fdfdccc67e9c36810933",
+    "url": "/building-permit-form/static/js/2.4222f9f6.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/building-permit-form/static/js/2.4222f9f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc6656a056e0b7a0bcf3",
+    "url": "/building-permit-form/static/js/main.607a5fbb.chunk.js"
+  },
+  {
+    "revision": "fb8d09e9a417496c3ed9",
+    "url": "/building-permit-form/static/js/runtime-main.cf45458f.js"
+  }
+]);
